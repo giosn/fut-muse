981967eaea94345ded21b220591d1fa48bb2eb08
Gio <giosndev@gmail.com>
2023-04-19 17:49:41 -0700

    chore(api): add player model

---
+++ api/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace fut_muse_api.Models
+{
+	public class Player
+	{
+		public int TMId { get; set; }
+		public string Name { get; set; }
+		public string? FullName { get; set; }
+		public DateTimeOffset DateOfBirth { get; set; }
+		public string PlaceOfBirth { get; set; }
+		public DateTimeOffset? DateOfDeath { get; set; }
+		public List<string> Citizenship { get; set; }
+		public int Age { get; set; }
+		public int? Height { get; set; } 
+		public string? PreferredFoot { get; set; }
+		public string Position { get; set; }
+		public string? CurrentClub { get; set; }
+		public string Status { get; set; }
+	}
+}
+
