f6b17dc1b20b9cadb072c9cb09aac0198a33e26f
Gio <giosndev@gmail.com>
2023-04-21 19:40:55 -0700

    chore(api): update player model

--- api/Models/Player.cs
+++ api/Models/Player.cs
@@ -5,17 +5,45 @@ namespace fut_muse_api.Models
 	{
 		public int TMId { get; set; }
 		public string Name { get; set; }
-		public string? FullName { get; set; }
-		public DateTimeOffset DateOfBirth { get; set; }
-		public string PlaceOfBirth { get; set; }
-		public DateTimeOffset? DateOfDeath { get; set; }
-		public List<string> Citizenship { get; set; }
+		public string FullName { get; set; }
+		public string? DateOfBirth { get; set; }
+		public string? PlaceOfBirth { get; set; }
+		public string? CountryOfBirth { get; set; }
+		public string? DateOfDeath { get; set; }
 		public int Age { get; set; }
-		public int? Height { get; set; } 
-		public string? PreferredFoot { get; set; }
+		public int Height { get; set; }
 		public string Position { get; set; }
 		public string? CurrentClub { get; set; }
-		public string Status { get; set; }
+		public string? Status { get; set; }
+
+		public Player(
+			int tmId,
+			string name,
+			string fullName,
+			string? dateOfBirth,
+			string? placeOfBirth,
+			string? countryOfBirth,
+			string? dateOfDeath,
+			int age,
+			int height,
+			string position,
+			string? currentClub,
+			string? status
+		)
+		{
+			TMId = tmId;
+			Name = name;
+			FullName = fullName;
+			DateOfBirth = dateOfBirth;
+			PlaceOfBirth = placeOfBirth;
+			CountryOfBirth = countryOfBirth;
+			DateOfDeath = dateOfDeath;
+			Age = age;
+			Height = height;
+			Position = position;
+			CurrentClub = currentClub;
+			Status = status;
+		}
 	}
 }
 
