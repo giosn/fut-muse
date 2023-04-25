6fbe3faf291259afee1f3d92d5943b52d8862af0
Gio <giosndev@gmail.com>
2023-04-25 12:49:12 -0700

    chore(api): add replace entity extension

---
+++ api/Extensions/ReplaceEntityExtension.cs
@@ -0,0 +1,30 @@
+﻿namespace fut_muse_api.Extensions
+{
+    public static class ReplaceEntityExtension
+    {
+        static readonly Dictionary<string, string> entityList = new()
+        {
+            { "Italienischer Zweitligameister", "Serie B Champion" },
+            { "Uefa", "UEFA" },
+            { "Europapokal der Pokalsieger Sieger", "UEFA Cup Winners' Cup" },
+            { "Under-17", "U17" },
+            { "Under-19", "U19" },
+            { "Under-20", "U20" },
+            { "Under-21", "U21" },
+            { "U20-Weltmeisterschaft", "U20 World Cup" }
+        };
+
+        public static string ReplaceEntity(this string entity)
+        {
+            var newEntity = entityList.FirstOrDefault(e => entity.ToLower().Contains(e.Key.ToLower()));
+
+            if (newEntity.Key is not null)
+            {
+                entity = entity.Replace(newEntity.Key, newEntity.Value);
+            }
+
+            return entity;
+        }
+    }
+}
+
