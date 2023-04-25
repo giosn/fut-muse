7ce6d95fd1ec95d018ca2d8cd3a2a5bbe49c5d09
Gio <giosndev@gmail.com>
2023-04-25 12:48:04 -0700

    chore(api): add achievement models
    Adds achievement and title models

---
+++ api/Models/Achievement.cs
@@ -0,0 +1,21 @@
+﻿namespace fut_muse_api.Models
+{
+	public class Achievement
+	{
+		public string Name { get; set; }
+		public int NumberOfTitles { get; set; }
+		public List<Title> Titles { get; set; }
+
+		public Achievement(
+			string name,
+			int numberOfTitles,
+			List<Title> titles
+		)
+		{
+			Name = name;
+			NumberOfTitles = numberOfTitles;
+			Titles = titles;
+		}
+	}
+}
+
---
+++ api/Models/Title.cs
@@ -0,0 +1,18 @@
+﻿namespace fut_muse_api.Models
+{
+	public class Title
+	{
+		public string Period { get; set; }
+		public string? Entity { get; set; }
+
+		public Title(
+			string period,
+			string? entity
+		)
+		{
+			Period = period;
+			Entity = entity;
+		}
+	}
+}
+
