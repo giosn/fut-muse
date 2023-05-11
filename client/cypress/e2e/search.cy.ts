9f46b83c691484e3e62c8cf56f2c84f4a712729a
Gio <giosndev@gmail.com>
2023-05-10 18:29:30 -0700

    test(client): add search page tests
    Tests the search results table

---
+++ client/cypress/e2e/search.cy.ts
@@ -0,0 +1,40 @@
+describe('Search page', () => {
+    beforeEach(() => {
+        cy.intercept('GET', `${Cypress.env('apiUrl')}/search/*`).as('getSearchResults');
+        cy.visit('search/messi');
+    });
+
+    it(`should navigate to the player page when a row's anchor is clicked`, () => {
+        cy.wait('@getSearchResults').then(() => {
+            cy.get(`[data-cy="search-anchor"]`)
+                .first()
+                .click();
+            cy.location().then(loc => {
+                expect(loc.href).to.contain('/player/');
+            })
+        });
+    });
+
+    it(`should get search results when the paginator buttons are clicked`, () => {
+        let totalHits: number;
+        cy.wait('@getSearchResults').then(interception => {
+            totalHits = interception.response!.body.totalHits;
+            cy.get(`[data-cy="search-paginator"] .mat-paginator-navigation-next`);
+            return cy.get(`[data-cy="search-paginator"] .mat-paginator-navigation-next`)
+                .click()
+                .wait('@getSearchResults');
+        }).then(interception => {
+            expect(interception.request.url).to.contain('?page=2');
+            return cy.get(`[data-cy="search-paginator"] .mat-paginator-navigation-previous`)
+                .click()
+                .wait('@getSearchResults');
+        }).then(interception => {
+            expect(interception.request.url).to.contain('?page=1');
+            return cy.get(`[data-cy="search-paginator"] .mat-paginator-navigation-last`)
+                .click()
+                .wait('@getSearchResults');
+        }).then(interception => {
+            expect(interception.request.url).to.contain(`?page=${Math.ceil(totalHits / 10)}`);
+        });
+    });
+});
